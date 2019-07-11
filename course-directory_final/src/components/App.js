7a3bc094e0c69ba3168cf459a11b405839a6c7b0
Hayk1509 <44315338+Hayk1509@users.noreply.github.com>
2019-07-11 15:03:36 +0400

    Add files via upload

---
+++ course-directory_final/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// App components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" render={ () => <About title='About' /> } />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
+
+
+
+
